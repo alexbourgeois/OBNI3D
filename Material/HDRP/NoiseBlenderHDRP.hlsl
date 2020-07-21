6a8626bfc60a89ddc0f4816025a34e2ac313c14a
alex_bourgeois <abourgeois27380@gmail.com>
2020-07-21 16:42:53 +0200

    [HDRP] Deformation subshader

--- Material/HDRP/NoiseBlenderHDRP.hlsl
+++ Material/HDRP/NoiseBlenderHDRP.hlsl
@@ -1,6 +1,6 @@
 
-#include "../Plugins/Unity-Noises/Includes/SimplexNoise3D.hlsl"
-#include "../Plugins/Unity-Noises/Includes/VoronoiNoise3D.hlsl"
+#include "../../Plugins/Unity-Noises/Includes/SimplexNoise3D.hlsl"
+#include "../../Plugins/Unity-Noises/Includes/VoronoiNoise3D.hlsl"
 
 int noiseVolumeCount = 0;
 float4x4 noiseVolumeTransforms[35]; //Max 35 volumes
@@ -72,8 +72,8 @@ float raymarchToEdge(uint volumeIndex, float3 direction, float3 position) {
 	return t;
 }
 
-float3 GetNoiseOnPosition(float4 vertex, float3 normal) {
-	float3 total = float3(0.0f,0.0f,0.0f);
+void GetNoiseOnPosition_float(float3 vertex, float3 normal, out float3 total) {
+	total = float3(0.0f, 0.0f, 0.0f);
 	float time = 0.0;
 
 	for (int i = 0; i < noiseVolumeCount * 28; i += 28) {
@@ -87,14 +87,12 @@ float3 GetNoiseOnPosition(float4 vertex, float3 normal) {
 		time = (noiseVolumeSettings[i + 9] == 1.0f) ? noiseVolumeSettings[i + 10] : _Time.y;
 		float currentNoiseValue = 0.0f;
 
-		float3 pos = float3(0, 0, 0);
-		//Position
-		if (noiseVolumeSettings[i + 19] == 1) {//World space
-			pos = mul(unity_ObjectToWorld, vertex).xyz;
-		}
+		float3 pos = vertex.xyz;
+
 		if (noiseVolumeSettings[i + 19] == 2) {//Noise space
 			pos = mul(vertex.xyz, currentVolumeTransform);
 		}
+
 		float coeff = 0.0f;
 		if (noiseVolumeSettings[i + 16] == 5) {
 			coeff = 1.0f;
@@ -136,7 +134,7 @@ float3 GetNoiseOnPosition(float4 vertex, float3 normal) {
 
 		currentNoiseValue += noiseVolumeSettings[i + 2]; //offset
 
-		float volCoeff = sdGlobal(noiseVolumeSettings[i + 15], float4(mul(unity_ObjectToWorld, vertex).xyz, 1), currentVolumeTransform); //inside negative, outside positive
+		float volCoeff = sdGlobal(noiseVolumeSettings[i + 15], float4(vertex.xyz, 1), currentVolumeTransform); //inside negative, outside positive
 		if (volCoeff < 0) {
 
 			float3 axis = float3(noiseVolumeSettings[i + 21], noiseVolumeSettings[i + 22], noiseVolumeSettings[i + 23]);
@@ -147,13 +145,13 @@ float3 GetNoiseOnPosition(float4 vertex, float3 normal) {
 
 			currentNoiseValue *= clamp(lerp(0, 1, volCoeff / (noiseVolumeSettings[i + 14] + 0.00001)), 0, 1);
 
-			if (noiseVolumeSettings[i + 13] == 1 ) { //clamp to volume
-				if (sdGlobal(noiseVolumeSettings[i + 15], float4(mul(unity_ObjectToWorld, vertex).xyz + direction * currentNoiseValue, 1), currentVolumeTransform) > 0) {
-					currentNoiseValue = raymarchToEdge(i, direction * sign(currentNoiseValue), mul(unity_ObjectToWorld, vertex).xyz) * sign(currentNoiseValue);
+			if (noiseVolumeSettings[i + 13] == 1) { //clamp to volume
+				if (sdGlobal(noiseVolumeSettings[i + 15], float4(pos + direction * currentNoiseValue, 1), currentVolumeTransform) > 0) {
+					currentNoiseValue = raymarchToEdge(i, direction * sign(currentNoiseValue),pos) * sign(currentNoiseValue);
 				}
 			}
 
-			if(noiseVolumeSettings[i + 16] == 0) {
+			if (noiseVolumeSettings[i + 16] == 0) {
 				total = currentNoiseValue * direction;
 				break;
 			}
@@ -178,13 +176,11 @@ float3 GetNoiseOnPosition(float4 vertex, float3 normal) {
 	//MASK
 	for (int j = 0; j < noiseVolumeCount * 28; j += 28) {
 		if (noiseVolumeSettings[j] == -1) { //mask
-			float volCoeff = sdGlobal(noiseVolumeSettings[j + 15], float4(mul(unity_ObjectToWorld, vertex).xyz, 1), noiseVolumeTransforms[(uint)(j / 27)]);
+			float volCoeff = sdGlobal(noiseVolumeSettings[j + 15], float4(vertex.xyz, 1), noiseVolumeTransforms[(uint)(j / 27)]);
 			volCoeff = -volCoeff;
 			volCoeff = max(volCoeff, 0);
 			float intensity = clamp(lerp(0, 1, volCoeff / (noiseVolumeSettings[j + 14] + 0.00001)), 0, 1);
-			total = lerp(total, float3(0.0f,0.0f,0.0f), intensity);
+			total = lerp(total, float3(0.0f, 0.0f, 0.0f), intensity);
 		}
 	}
-
-	return total;
 }
